14468db6c252d68732e3730223f4419277b071a3
vjchat-git <vbvijaibaskar@gmail.com>
2024-11-20 13:12:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 print("Current working directory:", os.getcwd())
-data = pd.read_excel("chatbotpoc.XLSX")
+data = pd.read_excel("chatbotpoc.xlsx")
 one_hot_encoder_dgroup = OneHotEncoder(sparse_output=False)
 one_hot_encoder_cgroup = OneHotEncoder(sparse_output=False)
 label_encoder_dcode = LabelEncoder()
